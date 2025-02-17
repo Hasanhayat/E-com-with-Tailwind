d33971c752526b3a4a9cb74cf8387063b08611c9
hasanhayat <aylskhsmvz.v@gmail.com>
2025-02-17 22:42:10 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
 import "./App.css";
 import logo from "./assets/logo.png";
+import hero from "./assets/hero-img.png";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="bg-gray-500 h-screen font-sans">
+    <div className="bg-theme min-h-screen font-sans">
       <div className="top w-screen h-8 bg-white items-center flex justify-around">
         <div className="left text-gray-800 ">
-          <ul className="list-none flex items-center flex gap-2 text-[12px] cursor-pointer hover:underline font-light">
+          <ul className="list-none flex items-center flex gap-2 text-xs cursor-pointer hover:underline font-light">
             <li>ENGLISH</li>
             <li>USD</li>
           </ul>
         </div>
         <div className="right text-gray-800">
-          <ul className="list-none flex items-center flex gap-4 text-[12px] cursor-pointer hover:underline font-light">
+          <ul className="list-none flex items-center flex gap-4 text-xs cursor-pointer hover:underline font-light">
             <li>My Account</li>
             <li>wishlish</li>
             <li>checkout</li>
@@ -23,13 +24,13 @@ function App() {
           </ul>
         </div>
       </div>
-      <nav className="w-screen min-h-16 bg-gray-800 flex justify-around items-center">
+      <nav className="w-screen min-h-16 bg-gray-800 flex justify-around items-center sticky top-0 ">
         <div className="logo">
           <img src={logo} alt="logo" />
         </div>
-        <div className="navlinks">
+        <div className="navlinks hidden md:block">
           {" "}
-          <ul className="text-sm list-none flex items-center gap-x-7 text-2 text-white font-light">
+          <ul className="text-sm list-none flex items-center gap-x-7 text-xs text-white font-light">
             <li className="hover:text-blue-300 transition-all duration-700 cursor-pointer">
               HOME
             </li>
@@ -54,6 +55,17 @@ function App() {
           </ul>
         </div>
       </nav>
+      <section className="hero max-h-[calc(100vh-64px)] flex items-center justify-around bg-theme overflow-hidden px-themePadding">
+         <div className="contant flex flex-col ">
+          <h4>New Arrivals</h4>
+          <h1>New Style For Lamps</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut provident ut cupiditate, distinctio repudiandae, ab architecto facere porro quo delectus neque accusamus, enim nesciunt impedit quas assumenda! Architecto, dolores? Exercitationem?</p>
+          <button>Shop Now</button>
+         </div>
+
+        <img src={hero} alt=""  className=""/>
+
+      </section>
     </div>
   );
 }
